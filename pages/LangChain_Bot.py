eda35f7c5a92f50b449afd7bf319babe0a68fd01
Park Lu <chochinlu@gmail.com>
2024-07-09 14:31:05 +0800

    add langchain bot

--- pages/LangChain_Bot.py
+++ pages/LangChain_Bot.py
@@ -1,7 +1,11 @@
+import uuid
 import streamlit as st
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage
-
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
 with st.sidebar:
     openai_api_key = st.text_input(
@@ -13,22 +17,40 @@ with st.sidebar:
 st.title("Basic Chatbot by LangChain")
 
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+msgs = StreamlitChatMessageHistory()
 
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+# Get an OpenAI API key before starting
+if not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.")
+    st.stop()
 
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are an AI chatbot having a conversation with a human."),
+        MessagesPlaceholder(variable_name="history"),
+        ("human", "{question}"),
+    ]
+)
+
+chain = prompt | ChatOpenAI(api_key=openai_api_key)
+
+
+# Set up langchain
+chain_with_history = RunnableWithMessageHistory(
+    chain,
+    lambda session_id: msgs,
+    input_messages_key="question",
+    history_messages_key="history",
+)
+
+# Render current messages from StreamlitChatMessageHistory
+for msg in msgs.messages:
+    st.chat_message(msg.type).write(msg.content)
+
+# If user inputs a new prompt, generate and draw a new response
 if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
-
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-
-    model = ChatOpenAI(api_key=openai_api_key, model="gpt-3.5-turbo")
-    response = model.invoke([HumanMessage(prompt)])
-    msg = response.content
-    st.session_state.messages.append({"role": "assistant", "content": msg})
-    st.chat_message("assistant").write(msg)
+    st.chat_message("human").write(prompt)
+    config = {"configurable": {"session_id": "any"}}
+    response = chain_with_history.invoke({"question": prompt}, config)
+    st.chat_message("ai").write(response.content)
