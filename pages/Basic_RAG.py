ab1d055f60f979e15dcb51beb0da983210208f52
Park Lu <chochinlu@gmail.com>
2024-07-12 10:51:22 +0800

    basic RAG

---
+++ pages/Basic_RAG.py
@@ -0,0 +1,65 @@
+import bs4
+import streamlit as st
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain import hub
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI
+
+if st.secrets["OPENAI_API_KEY"]:
+    openai_api_key = st.secrets["OPENAI_API_KEY"]
+else:
+    with st.sidebar:
+        openai_api_key = st.text_input(
+            "OpenAI API Key", key="chatbot_api_key", type="password"
+        )
+        "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+
+
+llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
+
+
+# Load, chunk and index the contents of the blog.
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    ),
+)
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+
+retriever = vectorstore.as_retriever()
+prompt = hub.pull("rlm/rag-prompt")
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+
+# Get an OpenAI API key before starting
+if not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.")
+    st.stop()
+
+
+if prompt := st.chat_input():
+    st.chat_message("human").write(prompt)
+    response = rag_chain.invoke(prompt)
+    st.chat_message("ai").write(response)
